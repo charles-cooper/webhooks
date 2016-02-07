e76ac714cbce40a28a78152bbc399e12f41d3f90
Charles Cooper <cooper.charles.m@gmail.com>
2016-02-07 00:16:41 -0500

    scratch the library
    .. start implementing from scratch.

--- src/Main.hs
+++ src/Main.hs
@@ -1,39 +1,71 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
 import Web.Scotty as S
+import Data.Aeson
+import Data.Aeson.TH
 
-import qualified Data.Text.Encoding   as T
 import qualified Data.Text.Lazy       as T
 import qualified Data.ByteString.Lazy as B
 
 import Data.Monoid
 
 import Control.Monad.IO.Class (liftIO)
-import GitHub.WebHook.Handler as Git
 
-deriving instance Show Git.Error
+import GHC.Generics
 
-scottyHandler :: [String] -> Handler ActionM
-scottyHandler keys = Handler keys getBody getHeader where
-  getBody = do
-    ret <- S.body
-    liftIO $ putStrLn $ "debug Body: "
-    liftIO $ B.putStrLn ret
-    return $ B.toStrict ret
-  getHeader key = do
-    liftIO $ putStr $ "get Header "
-    liftIO $ print $ key
-    val <- (fmap . fmap) (T.encodeUtf8 . T.toStrict) . S.header . T.fromStrict . T.decodeUtf8 $ key
-    liftIO $ putStr "got Header: "
-    liftIO $ print $ maybe "Nothing" id val
-    return val
+-------------------------
+-- Github webhook headers
+-------------------------
+
+githubEvent    :: T.Text
+githubEvent    = "X-Github-Event"
+
+hubSignature   :: T.Text
+hubSignature   = "X-Hub-Signature"
+
+githubDelivery :: T.Text
+githubDelivery = "X-Github-Delivery"
+ 
+--------------------------
+-- Github webhook payloads
+--------------------------
+
+data WebhookEvent
+  = Ping PingEvent
+  | Push PushEvent
+  deriving (Generic, Show)
+
+data PingEvent = PingEvent
+  { _pingzen     :: T.Text
+  , _pinghook_id :: Integer
+  -- unimplemented: ping hook
+  } deriving (Generic, Show)
+
+type GitRef = T.Text -- GitRef { unGitRef :: T.Text } deriving Show
+
+data PushEvent = PushEvent
+  { _pushref     :: T.Text
+  , _pushbefore  :: GitRef
+  , _pushafter   :: GitRef
+  , _pushcreated :: Bool
+  , _pushdeleted :: Bool
+  , _pushforced  :: Bool
+  -- unimplemented fields
+  } deriving (Generic, Show)
+
+instance FromJSON PushEvent where
+  parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = drop 5 }
 
 main :: IO ()
 main = scotty 3000 $ do
-  matchAny "/" $ do
-    res <- runHandler (scottyHandler [])
-    case res of
-      Left err -> liftIO $ print err
-      Right (uuid, payload) -> liftIO $ print payload
+  post "/" $ do
+    evtTy <- header githubEvent
+    res <- case evtTy of
+      Nothing     -> raise $ "Header '" <> githubEvent <> "' not found"
+      Just "push" -> Right . Push <$> jsonData
+      Just e      -> pure . Left $ "Unimplemented webhook event: " <> e
+    liftIO $ print res
+
