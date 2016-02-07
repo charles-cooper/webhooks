f0c4b201ad2bbb43309b57ae4643898de2c55fbe
Charles Cooper <cooper.charles.m@gmail.com>
2016-02-06 22:29:39 -0500

    debug body

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,8 @@ import qualified Data.Text.Encoding   as T
 import qualified Data.Text.Lazy       as T
 import qualified Data.ByteString.Lazy as B
 
+import Data.Monoid
+
 import Control.Monad.IO.Class (liftIO)
 import GitHub.WebHook.Handler as Git
 
@@ -15,7 +17,10 @@ deriving instance Show Git.Error
 
 scottyHandler :: [String] -> Handler ActionM
 scottyHandler keys = Handler keys getBody getHeader where
-  getBody = B.toStrict <$> S.body
+  getBody = do
+    ret <- S.body
+    liftIO $ print $ "debug body: " <> ret
+    return $ B.toStrict ret
   getHeader = (fmap . fmap) (T.encodeUtf8 . T.toStrict) . S.header . T.fromStrict . T.decodeUtf8
 
 main :: IO ()
