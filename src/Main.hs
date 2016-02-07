01a194db57900c6f127da72f869bf7b4d2ef1a0c
Charles Cooper <cooper.charles.m@gmail.com>
2016-02-07 00:02:20 +0000

    compile simple scotty app

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Web.Scotty as S
+
+import qualified Data.Text.Encoding   as T
+import qualified Data.Text.Lazy       as T
+import qualified Data.ByteString.Lazy as B
+
+import Control.Monad.IO.Class (liftIO)
+import GitHub.WebHook.Handler
+
+scottyHandler :: [String] -> Handler ActionM
+scottyHandler keys = Handler keys getBody getHeader where
+  getBody = B.toStrict <$> S.body
+  getHeader = (fmap . fmap) (T.encodeUtf8 . T.toStrict) . S.header . T.fromStrict . T.decodeUtf8
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = scotty 3000 $ do
+  matchAny "/" $ do
+    res <- runHandler (scottyHandler [])
+    case res of
+      Left err -> liftIO $ print "derp"
+      Right (uuid, paylod) -> liftIO $ print "Hey"
